aa1571ad7fa1d054972cd25092f6fd6fbaf9038b
Andrew Chen <deweichen47@gmail.com>
2015-07-10 19:50:49 -0400

    writing to csv

--- hackernews_api.py
+++ hackernews_api.py
@@ -1,23 +1,30 @@
 #!/usr/local/bin/python
 
 import os, sys
+import csv
 import requests
 
 BASE_URL = "https://hacker-news.firebaseio.com/v0"
-ITEM = BASE_URL + "/item/{0}.json?print=pretty"
-MAXITEM = BASE_URL + "/maxitem.json?print=pretty"
+ITEM = BASE_URL + "/item/{0}.json"
+MAXITEM = BASE_URL + "/maxitem.json"
 
-def get_request(url):
-	"Make GET request to the API for a single item"
-	r = requests.get(url)
-	if r.status_code == 200:
-		return (r.json(), r.text)
-	else:
-		print "Failed: status_code " + r.status_code
-		return (None, None)
+def get_request(url, text=True):
+    "Make GET request to the API for a single item. Set text to False to return dict"
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.text if text else r.json()
+    else:
+        print "Failed: status_code " + r.status_code
+        return None
+
 
 def main():
-	print get_request(ITEM.format(1))[1]
+    fieldnames = ["by", "descendants", "id", "kids", "score", "time", "title", "type", "url"]
+    with open("output.csv", "w") as csvfile:
+        max_id = get_request(MAXITEM)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(get_request(ITEM.format(1), text=False))
+
 
 if __name__ == '__main__':
-	main()
+    main()
