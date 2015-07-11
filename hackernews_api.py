8f4e4e07fe61974f21a4225f5bbe2257f1d9bf7d
Andrew Chen <deweichen47@gmail.com>
2015-07-10 20:28:09 -0400

    done testing

--- hackernews_api.py
+++ hackernews_api.py
@@ -2,15 +2,17 @@
 
 import os, sys
 import csv
+import re
 import requests
 
 BASE_URL = "https://hacker-news.firebaseio.com/v0"
-ITEM = BASE_URL + "/item/{0}.json"
-MAXITEM = BASE_URL + "/maxitem.json"
+ITEM_URL = BASE_URL + "/item/{0}.json"
+MAXITEM_URL = BASE_URL + "/maxitem.json"
 
 def get_request(url, text=True):
     "Make GET request to the API for a single item. Set text to False to return dict"
     r = requests.get(url)
+    r.encoding = "utf-8"
     if r.status_code == 200:
         return r.text if text else r.json()
     else:
@@ -19,11 +21,22 @@ def get_request(url, text=True):
 
 
 def main():
-    fieldnames = ["by", "descendants", "id", "kids", "score", "time", "title", "type", "url"]
+    fieldnames = ["id", "deleted", "type", "by", "time", "text", "dead", "parent", 
+                  "kids", "url", "score", "title", "parts", "descendants"]
     with open("output.csv", "w") as csvfile:
-        max_id = get_request(MAXITEM)
+        max_id = get_request(MAXITEM_URL)
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow(get_request(ITEM.format(1), text=False))
+        writer.writeheader()
+        for i in xrange(1, 100):
+            obj = get_request(ITEM_URL.format(i), text=False)
+            #do some clean up here
+            for key, value in obj.iteritems():
+                if isinstance(value, str) or isinstance(value, unicode):
+                    value = re.sub(r"[\n\t]", " ", value)
+                if isinstance(value, unicode):
+                    obj[key] = value.encode("utf-8")
+            #write to csv
+            writer.writerow(obj)
 
 
 if __name__ == '__main__':
