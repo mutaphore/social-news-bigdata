98e61039d22df8fc4c801f4dede64302182ab0b6
mutaphore <deweichen47@gmail.com>
2015-07-26 00:24:50 -0400

    compiling

--- mapreduce/url_type/src/VoteCountDriver.java
+++ mapreduce/url_type/src/VoteCountDriver.java
@@ -11,28 +11,28 @@ public class VoteCountDriver {
     public static void main(String[] args) throws Exception{
 
         if (args.length != 2) {
-            System.err.println("Usage: PageRankDriver <input path> <output path>");
+            System.err.println("Usage: VoteCountDriver <input path> <output path>");
             System.exit(-1);
         }
 
         Job job = new Job();
 
-        job.setJarByClass(PageRankDriver.class);
-        job.setJobName("Page Rank");
+        job.setJarByClass(VoteCountDriver.class);
+        job.setJobName("VoteCount");
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setMapperClass(PageRankMapper.class);
-        job.setReducerClass(PageRankReducer.class);
+        job.setMapperClass(VoteCountMapper.class);
+        job.setReducerClass(VoteCountReducer.class);
 
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(TextArrayWritable.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(IntWritable.class);
 
-        job.setOutputKeyClass(Text.class);
+        job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(FloatWritable.class);
         
-        job.setOutputFormatClass(PageRankOutputFormat.class);
+        // job.setOutputFormatClass(PageRankOutputFormat.class);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
